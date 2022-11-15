689443f5c8aa6540438ebbfb8e5cfc3db6cc11c0
ldcnft <101517909+ldcnft@users.noreply.github.com>
2022-11-15 09:34:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -157,8 +157,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 50) {
-      newMintAmount = 50;
+    if (newMintAmount > 2) {
+      newMintAmount = 2;
     }
     setMintAmount(newMintAmount);
   };
@@ -378,56 +378,6 @@ function App() {
                     </s.Container>
 
 <s.SpacerMedium />
-                        
-<s.Container ai={"center"} jc={"center"} fd={"row"}>
-
-  
-  
-  
-                        <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          tenMintAmount();
-                          getData();
-                        }}
-                      >
-                        10
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          twentyMintAmount();
-                          getData();
-                        }}
-                      >
-                        20
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          thirtyMintAmount();
-                          getData();
-                        }}
-                      >
-                        30
-                      </StyledRoundButton>
-<s.SpacerMedium />
-                      <StyledRoundButton
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          maxMintAmount();
-                          getData();
-                        }}
-                      >
-                        Max
-                      </StyledRoundButton>
-  
   
   
   
